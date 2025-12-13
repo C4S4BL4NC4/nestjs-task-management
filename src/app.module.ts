dcfba85ef5033096e7d0080fbdd9ff70eb79b9ee
C4S4BL4NC4 <74897764+C4S4BL4NC4@users.noreply.github.com>
2025-12-13 06:23:44 +0300

    feat(tasks): add Task entity and repository
    - Created Task entity with fields: id, title, description, and status.
    - Implemented TasksRepository extending TypeORM's Repository for Task entity.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,22 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as dotenv from 'dotenv';
+dotenv.config({ path: 'config.env' });
 
 @Module({
-  imports: [TasksModule],
+  imports: [
+    TasksModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: 5432,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
 })
 export class AppModule {}
