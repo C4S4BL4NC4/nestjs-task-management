b197b7a442cd63e0fa3753f496e265d9bde422b7
C4S4BL4NC4 <74897764+C4S4BL4NC4@users.noreply.github.com>
2026-01-04 00:29:46 +0300

    feat(config): add application and database configuration files
    - Created app.config.ts to define application configuration.
    - Added config.types.ts to establish validation schema using Joi.
    - Implemented database.config.ts for TypeORM configuration with PostgreSQL.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,20 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import * as dotenv from 'dotenv';
-dotenv.config({ path: 'config.env' });
+import { ConfigModule } from '@nestjs/config';
+import { appConfigSchema } from './config/config.types';
+import { typeOrmConfig } from './config/database.config';
 
 @Module({
   imports: [
-    TasksModule,
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DB_HOST,
-      port: 5432,
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE,
-      autoLoadEntities: true,
-      synchronize: true,
+    ConfigModule.forRoot({
+      load: [typeOrmConfig],
+      validationSchema: appConfigSchema,
+      validationOptions: {
+        // allowUnknown: false,
+        abortEarly: true,
+      },
     }),
+    TasksModule,
   ],
 })
 export class AppModule {}
